4bb6d6906d26314d002899c8e655b82746e73830
rex-remind101 <rex@remind101.com>
2017-11-02 21:17:39 -0700

    [master] swift 4 package

---
+++ Package@swift-4.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "CGraphQLParser"
+
+    name: "CGraphQLParser",
+    products: [
+        .library(
+            name: "CGraphQLParser",
+            targets: ["CGraphQLParser"]),
+        ],
+    targets: [
+        .target(
+            name: "CGraphQLParser",
+            dependencies: []),
+        ]
+)
